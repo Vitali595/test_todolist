1b8a5cff616c294594c939fa7430d925c95b231b
DavidPoshelyk <davidposhelyk@gmail.com>
2022-02-25 12:49:05 +0300

    Commit 2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <HashRouter>
             <div className={style.app}>
                 <Todolist/>
+                <>nj</>
             </div>
         </HashRouter>
     </>
